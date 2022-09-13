db100360d57e22009c6a0ba3cbb2730f59f6d1a0
heyethereum <heyethereum@gmail.com>
2022-09-13 23:25:52 +0800

    dom manipulation

--- Javascript Fundamentals/Add Delete/app.js
+++ Javascript Fundamentals/Add Delete/app.js
@@ -1,25 +1,33 @@
 function addItem() {
-   let text = document.getElementById('newText').value;
-   let list = document.getElementById('items');
+  let text = document.getElementById("newText").value;
+  let list = document.getElementById("items");
 
-   if(text.length == 0) {
-       return;
-   }
-   let li = document.createElement('li');
+  if (text.length == 0) {
+    return;
+  }
+  let li = document.createElement("li");
 
-   li.textContent = text;
+  li.textContent = text;
 
-   let removeA = document.createElement('a');
-   let link = document.createTextNode('[Delete]');
-   removeA.appendChild(link);
-   removeA.href = '#';
+  let removeA = document.createElement("a");
+  let link = document.createTextNode("[Delete]");
+  removeA.appendChild(link);
+  removeA.href = "#";
 
-   removeA.addEventListener('click', deleteItem);
-   li.appendChild(removeA);
-   list.appendChild(li);
+  removeA.addEventListener("click", deleteItem);
+  li.appendChild(removeA);
+  list.appendChild(li);
 
-
-   function deleteItem(){
-       li.remove()
-   }
+  function deleteItem() {
+    li.remove();
+  }
 }
+
+const input = document.getElementById("newText");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    document.getElementById("addBtn").click();
+  }
+});
