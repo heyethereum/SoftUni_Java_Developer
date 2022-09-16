bfda1af685c871ce85588ee23a27275cda94c3cd
heyethereum <heyethereum@gmail.com>
2022-09-16 22:24:45 +0800

    internal object properties

--- Javascript Fundamentals/Person-Internal-Object.js
+++ Javascript Fundamentals/Person-Internal-Object.js
@@ -1,21 +1,26 @@
-function Person(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+class Person {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
 
-  Object.defineProperty(this, "fullname", {
-    set: function (value) {
-      let name = value.split(" ");
+    Object.defineProperty(this, "fullname", {
+      set: function (value) {
+        let name = value.split(" ");
 
-      if (name.length === 2) {
-        this.firstName = name[0];
-        this.lastName = name[1];
-      }
-    },
-    get: function () {
-      return `${this.firstName} ${this.lastName}`;
-    },
-  });
+        if (name.length === 2) {
+          this.firstName = name[0];
+          this.lastName = name[1];
+        }
+      },
+      get: function () {
+        return `${this.firstName} ${this.lastName}`;
+      },
+    });
+  }
 }
+
+let person = new Person("Peter", "Smith");
+console.log(person.fullname); // Peter Smith
 
 const users = [{ name: "piggy" }, { name: "puppy" }, { name: "kitty" }];
 const getName = (user) => user.name;
