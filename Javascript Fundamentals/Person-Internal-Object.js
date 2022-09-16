94694b466c79e40b38b94b7eb4a63f2bdcd00459
heyethereum <heyethereum@gmail.com>
2022-09-16 22:41:42 +0800

    internal object properties

--- Javascript Fundamentals/Person-Internal-Object.js
+++ Javascript Fundamentals/Person-Internal-Object.js
@@ -3,8 +3,8 @@ class Person {
     this.firstName = first;
     this.lastName = last;
 
-    Object.defineProperty(this, "fullname", {
-      set: function (value) {
+    Object.defineProperty(this, "fullName", {
+      set: (value) => {
         let name = value.split(" ");
 
         if (name.length === 2) {
@@ -12,15 +12,33 @@ class Person {
           this.lastName = name[1];
         }
       },
-      get: function () {
-        return `${this.firstName} ${this.lastName}`;
-      },
+      get: () => `${this.firstName} ${this.lastName}`,
     });
   }
 }
 
 let person = new Person("Peter", "Smith");
-console.log(person.fullname); // Peter Smith
+console.log(person.fullName); // Peter Smith
+
+person.firstName = "George";
+console.log(person.fullName); // George Smith
+
+person.lastName = "Peterson";
+console.log(person.fullName); // George Peterson
+
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); // Nikola
+console.log(person.lastName); // Tesla
+
+let person2 = new Person("Albert", "Simpson");
+console.log(person2.fullName); // Albert Simpson
+
+person2.firstName = "Simon";
+console.log(person2.fullName); // Simon Simpson
+
+person2.fullName = "Peter";
+console.log(person2.firstName); // Simon
+console.log(person2.lastName);
 
 const users = [{ name: "piggy" }, { name: "puppy" }, { name: "kitty" }];
 const getName = (user) => user.name;
